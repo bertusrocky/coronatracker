e574ef32aa3b215a56a33bb1d393c9fbb79f159e
Adam Bertus <bertus.rocky@gmail.com>
2020-05-11 18:01:48 +0100

    final version

--- app.js
+++ app.js
@@ -1,6 +1,73 @@
+// DEFAULT COUNTRY
 let country = 'United Kingdom';
 
-// GET STATISTICS
+// GLOBAL CHART STYLES
+Chart.defaults.global.defaultFontSize	= 16;
+Chart.defaults.global.defaultFontFamily	= "'Roboto', 'sans-serif'";
+
+// GET GLOBAL DATA
+async function getGlobal() {
+  const response = await fetch('https://covid19.mathdro.id/api');
+  const responseData = await response.json();
+
+  let confirmed = responseData.confirmed.value;
+  let deaths = responseData.deaths.value;
+  let recovered = responseData.recovered.value;
+
+  //PIE CHART
+  let myChart = document.getElementById("globalChart").getContext("2d");
+  let globalChart = new Chart(myChart, {
+    type:'doughnut',
+    data:{
+      labels:['COVID-19 Cases', 'Deaths', 'Recovered'],
+      datasets:[{
+        label: 'Global', 
+        backgroundColor: [
+          'rgba(255,165,0, 0.7)', 
+          'rgb(255,0,0, 0.7)',
+          'rgba(0,128,0, 0.7)'
+        ],
+        data: [confirmed, deaths, recovered]
+      }]
+    },
+    options:{
+      title:{
+        display: false,
+        text: 'Global Cases',
+        fontSize: 26
+      },
+      legend:{
+        display: true,
+        position: 'left',
+        align: 'center',
+        labels:{
+          fontSize: 16,
+          fontColor: '#333333',
+          fontStyle: '600'
+        }
+      }
+    }
+  });
+  
+  // COUNTING NUMBERS
+  let options = {
+    useEasing: true,
+    useGrouping: true,
+    separator: ",",
+    decimal: '.',
+    };
+    
+    let confirmedCountUp = new CountUp('globalConfirmed', 0, confirmed, 0, 3, options);
+    let deathsCountUp= new CountUp('globalDeaths', 0, deaths, 0, 3, options);
+    let recoveredCountUp = new CountUp('globalRecovered', 0, recovered, 0, 3, options);
+    
+    confirmedCountUp.start();
+    deathsCountUp.start();
+    recoveredCountUp.start();
+}
+
+
+// GET COUNTRY STATS
 async function getStats() {
 const response = await fetch(`https://covid19.mathdro.id/api/countries/${country}`);
 const responseData = await response.json();
@@ -8,7 +75,7 @@ const responseData = await response.json();
 let confirmed = responseData.confirmed.value;
 let deaths = responseData.deaths.value;
 let recovered = responseData.recovered.value;
-let lastUpdate = new Date(responseData.lastUpdate).toDateString();
+let lastUpdate = "Last updated: " + new Date(responseData.lastUpdate).toGMTString();
 
 // DATE
 document.getElementById('lastUpdate').innerHTML = lastUpdate;
@@ -29,7 +96,7 @@ confirmedCountUp.start();
 deathsCountUp.start();
 recoveredCountUp.start();
 
-// CHART
+// BAR CHART
 let myChart = document.getElementById("myChart").getContext("2d");
   if(window.bar != undefined) 
   window.bar.destroy(); 
@@ -40,9 +107,9 @@ let myChart = document.getElementById("myChart").getContext("2d");
       datasets:[{
         label: `Currently in ${country}`, 
         backgroundColor: [
-          'rgba(8,81,245, 0.6)', 
-          'rgba(241,79,96, 0.6)',
-          'rgba(34,169,113, 0.6)'
+          'rgba(255,165,0, 0.7)', 
+          'rgb(255,0,0, 0.7)',
+          'rgba(0,128,0, 0.7)'
         ],
         data: [confirmed, deaths, recovered]
       }]
@@ -50,8 +117,10 @@ let myChart = document.getElementById("myChart").getContext("2d");
     options:{
       title:{
         display: true,
-        text: `Active cases, deaths and recoveries in ${country}`,
-        fontSize: 22
+        text: `${country}`,
+        fontSize: 23,
+        fontFamily: "'Roboto', sans-serif",
+        fontColor: '#333333'
       },
       legend:{
         display: false
@@ -66,12 +135,9 @@ function getCountries() {
     .then(function(resp) {
         return resp.json();
     })
-
-  // Map through countries object and get the country names
     .then(function(data) {
         let countryList = data.countries.map((country) => country.name);
 
-  // Create options for select element
         for( let i = 0; i < countryList.length; i++) {
           let option = countryList[i];
           let element = document.createElement('option');
@@ -83,6 +149,7 @@ function getCountries() {
     .catch(err => console.log(err));
   }
 
+
 // CHANGE LOCATION
 document.getElementById('countries').addEventListener('change', (e) => {
   
@@ -93,5 +160,7 @@ document.getElementById('countries').addEventListener('change', (e) => {
 
 });
 
+// RUN FUNCTIONS WHEN CONTENT IS LOADED
+document.addEventListener('DOMContentLoaded', getGlobal);
 document.addEventListener('DOMContentLoaded', getCountries);
 document.addEventListener('DOMContentLoaded', getStats);
